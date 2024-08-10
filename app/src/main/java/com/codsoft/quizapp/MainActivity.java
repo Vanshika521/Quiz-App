96a1e9e57039494f3c1eff1ab5aa8650564014e6
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2024-08-10 23:30:19 +0530

    Main file and Coding interface

--- app/src/main/java/com/codsoft/quizapp/MainActivity.java
+++ app/src/main/java/com/codsoft/quizapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.codsoft.quizapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void C(View view) {
+    }
+
+    public void c_plusplus(View view) {
+    }
+
+    public void JAVA(View view) {
+    }
+
+    public void Python(View view) {
+    }
+
+    public void Kotlin(View view) {
+    }
+
+    public void HTML(View view) {
     }
 }
